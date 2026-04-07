7e3c967027bdb968bf5d849612d088f79bdf3f93
sandaruwankavishka376-alt <sandaruwankavishka376@gmail.com>
2026-04-07 14:22:36 +0530

    Fix SESSION_ID assignment in settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === 𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=5ba122a026085628 ? 'Your SESSION ID Put' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
